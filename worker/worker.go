c76b4c6dc6ced6739341e0f18cda7f03edadc3f9
melijauregui <mjauregui@fi.uba.ar>
2025-04-23 22:53:01 -0300

    [wip] no anda

--- worker/worker.go
+++ worker/worker.go
@@ -152,8 +152,8 @@ func (t *SourceTask) Connect(middlewareConnection middleware.MiddlewareCola[comm
 
 func NewWorker() Worker {
 	movies_metadata := NewSourceTask("movies_metadata")
-	credits := NewSourceTask("credits")
-	// ratings := NewSourceTask("ratings")
+	// credits := NewSourceTask("credits")
+	ratings := NewSourceTask("ratings")
 	movies_metadata_clean := clean.NewCleanMovies(movies_metadata, []string{"filter_release_date_ge_2000_and_include_ar", "filter_one_production_country", "map_sentiment_rate"})
 	n_worker, err := strconv.Atoi(os.Getenv("N_JOINERS")) // TODO: cambiar en el compose
 	if err != nil {
@@ -165,14 +165,14 @@ func NewWorker() Worker {
 		joiner_credits_subscribers = append(joiner_credits_subscribers, fmt.Sprintf("joiner_%d_credits", i+1))
 		joiner_ratings_subscribers = append(joiner_ratings_subscribers, fmt.Sprintf("joiner_%d_ratings", i+1))
 	}
-	credits_clean := clean.NewCleanCredits(credits, joiner_credits_subscribers)
-	// ratings_clean := clean.NewCleanRatings(ratings, joiner_ratings_subscribers)
+	// credits_clean := clean.NewCleanCredits(credits, joiner_credits_subscribers)
+	ratings_clean := clean.NewCleanRatings(ratings, joiner_ratings_subscribers)
 
 	filter_release_date_ge_2000_and_include_ar := filter.NewFilterReleaseDateGe2000AndIncludeAR(movies_metadata_clean.Name(), []string{"filter_release_date_l_2010_and_include_es", "joiner_credits","joiner_ratings"})
-	filter_release_date_l_2010_and_include_es := filter.NewFilterReleaseDateL2010AndIncludeES(filter_release_date_ge_2000_and_include_ar.Name(), []string{"q1"})
-	filter_one_production_country := filter.NewFilterProductionCountriesLen1(movies_metadata_clean.Name(), []string{"reduce_by_country_sum_budget"})
-	joiner_credits := joiner.NewJoinerCredits(filter_release_date_ge_2000_and_include_ar, credits_clean, []string{"reduce_by_actor"})
-	// joiner_ratings := joiner.NewJoinerRatings(filter_release_date_ge_2000_and_include_ar, ratings_clean, []string{"q4"})
+	// filter_release_date_l_2010_and_include_es := filter.NewFilterReleaseDateL2010AndIncludeES(filter_release_date_ge_2000_and_include_ar.Name(), []string{"q1"})
+	// filter_one_production_country := filter.NewFilterProductionCountriesLen1(movies_metadata_clean.Name(), []string{"reduce_by_country_sum_budget"})
+	// joiner_credits := joiner.NewJoinerCredits(filter_release_date_ge_2000_and_include_ar, credits_clean, []string{"reduce_by_actor"})
+	joiner_ratings := joiner.NewJoinerRatings(filter_release_date_ge_2000_and_include_ar, ratings_clean, []string{"q3"})
 
 	// grpcAddress := os.Getenv("NLP_GRPC_ADDR")
 
@@ -182,13 +182,13 @@ func NewWorker() Worker {
 	return Worker{
 		Tasks: []task.Task{
 			movies_metadata_clean,
-			// ratings_clean,
-			credits_clean,
+			ratings_clean,
+			// credits_clean,
 			filter_release_date_ge_2000_and_include_ar,
-			filter_release_date_l_2010_and_include_es,
-			filter_one_production_country,
-			joiner_credits,
-			// joiner_ratings,
+			// filter_release_date_l_2010_and_include_es,
+			// filter_one_production_country,
+			// joiner_credits,
+			joiner_ratings,
 			// map_nlp,
 			// filter_avg_rate,
 		},
