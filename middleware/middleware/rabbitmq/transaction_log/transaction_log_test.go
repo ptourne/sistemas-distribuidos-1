49789d23d3bb8653493a3212420b76e19c8d5364
Patricio Tourne Passarino <ptourne@fi.uba.ar>
2025-05-24 15:12:01 -0300

    fix: logger simplification

--- middleware/middleware/rabbitmq/transaction_log/transaction_log_test.go
+++ middleware/middleware/rabbitmq/transaction_log/transaction_log_test.go
@@ -2,7 +2,6 @@ package transaction_log
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,10 +17,8 @@ func TestTransactionLog(t *testing.T) {
 	t.Run("Test Opened Transaction", func(t *testing.T) {
 		testWithOps(t, []Op{
 			{received: &received{1, []byte("data")}},
-		}, func(tl *TransactionLog) {
-			assert.Equal(t, map[uint64][]byte{
-				1: []byte("data"),
-			}, tl.OpenTransactions())
+		}, func(tl TransactionLog) {
+			assert.Equal(t, &Transaction{Id: 1, Data: []byte("data")}, tl.OpenedTransaction())
 		})
 	})
 
@@ -29,8 +26,9 @@ func TestTransactionLog(t *testing.T) {
 		testWithOps(t, []Op{
 			{received: &received{1, []byte("data")}},
 			{acknowledged: &acknowledged{1}},
-		}, func(tl *TransactionLog) {
-			assert.Equal(t, map[uint64][]byte{}, tl.OpenTransactions())
+		}, func(tl TransactionLog) {
+			assert.Nil(t, tl.OpenedTransaction())
+			assert.True(t, tl.IsDuplicate(1))
 		})
 	})
 
@@ -40,10 +38,8 @@ func TestTransactionLog(t *testing.T) {
 			{acknowledged: &acknowledged{1}},
 			{received: &received{3, []byte("data")}},
 			{acknowledged: &acknowledged{3}},
-		}, func(tl *TransactionLog) {
-			assert.Equal(t, map[uint64][]byte{}, tl.OpenTransactions())
-			assert.Equal(t, 1, tl.closedNonContiguousTransactions.Len())
-			assert.Equal(t, &contiguousBlock{3, 3}, tl.closedNonContiguousTransactions.Front().Value.(*contiguousBlock))
+		}, func(tl TransactionLog) {
+			assert.True(t, tl.IsDuplicate(1))
 		})
 	})
 
@@ -52,23 +48,23 @@ func TestTransactionLog(t *testing.T) {
 			{received: &received{1, []byte("data")}},
 			{acknowledged: &acknowledged{1}},
 			{received: &received{3, []byte("data")}},
-			{received: &received{4, []byte("data")}},
 			{acknowledged: &acknowledged{3}},
-			{acknowledged: &acknowledged{4}},
-		}, func(tl *TransactionLog) {
-			assert.Equal(t, map[uint64][]byte{}, tl.OpenTransactions())
-			assert.Equal(t, 1, tl.closedNonContiguousTransactions.Len())
-			assert.Equal(t, &contiguousBlock{3, 4}, tl.closedNonContiguousTransactions.Front().Value.(*contiguousBlock))
+			{received: &received{4, []byte("data")}},
+		}, func(tl TransactionLog) {
+			assert.True(t, tl.IsDuplicate(1))
+			assert.False(t, tl.IsDuplicate(2))
+			assert.True(t, tl.IsDuplicate(3))
+			assert.False(t, tl.IsDuplicate(4))
 		})
 	})
 }
 
-func testWithOps(t *testing.T, ops []Op, tests func(tl *TransactionLog)) {
+func testWithOps(t *testing.T, ops []Op, tests func(tl TransactionLog)) {
 	// Create a mock file
 	f := newMockFile()
 
 	// Create a new TransactionLog instance
-	tl := NewTransactionLog(f)
+	tl := NewTransactionLog(f, false)
 
 	for _, op := range ops {
 		var err error
@@ -85,12 +81,12 @@ func testWithOps(t *testing.T, ops []Op, tests func(tl *TransactionLog)) {
 	tests(tl)
 
 	// Check output
-	fmt.Printf("File: %X\n", f.Bytes())
-	fmt.Printf("File: %s\n", f.String())
+	t.Logf("File: %X\n", f.Bytes())
+	t.Logf("File: %s\n", f.String())
 
 	// r := bytes.NewBuffer(f)
 	nw := newMockFile()
-	recoveredTL := NewTransactionLogFrom(f, nw)
+	recoveredTL := NewTransactionLogFrom(f, nw, false)
 	assert.NotNil(t, recoveredTL, "Expected TransactionLog to be created from file")
 	t.Logf("RecoveredTransactionLog: %+v", recoveredTL)
 
@@ -99,20 +95,22 @@ func testWithOps(t *testing.T, ops []Op, tests func(tl *TransactionLog)) {
 	tests(recoveredTL)
 }
 
-func AssertEqualTL(t *testing.T, tl *TransactionLog, recoveredTL *TransactionLog) {
-	assert.Equal(t, tl.openedTransactions, recoveredTL.openedTransactions, "Expected opened transactions to match")
-	assert.Equal(t, tl.closedNonContiguousTransactions, tl.closedNonContiguousTransactions, "Expected closed non-contiguous transactions to match")
-	assert.Equal(t, tl.lastContiguousClosedTransaction, tl.lastContiguousClosedTransaction, "Expected last contiguous closed transaction to match")
+func AssertEqualTL(t *testing.T, tl TransactionLog, recoveredTL TransactionLog) {
+	if otl, ok := tl.(*orderedTransactionLog); ok {
+		recoveredOtl, ok := recoveredTL.(*orderedTransactionLog)
+		assert.True(t, ok, "Expected recovered TransactionLog to be of type orderedTransactionLog")
+		assert.Equal(t, otl.openedTransaction, recoveredOtl.openedTransaction, "Expected opened transactions to match")
+		assert.Equal(t, otl.lastClosedTransaction, recoveredOtl.lastClosedTransaction, "Expected last closed transaction to match")
+	} else if dtl, ok := tl.(*disorderedTransactionLog); ok {
+		recoveredDtl, ok := recoveredTL.(*disorderedTransactionLog)
+		assert.True(t, ok, "Expected TransactionLog to be of type disorderedTransactionLog")
+		assert.Equal(t, dtl.openedTransaction, recoveredDtl.openedTransaction, "Expected opened transactions to match")
+		assert.Equal(t, dtl.closedTransactions, recoveredDtl.closedTransactions, "Expected last contiguous closed transaction to match")
+	} else {
+		t.Fatalf("Expected TransactionLog to be of type orderedTransactionLog or disorderedTransactionLog, %+v", tl)
+	}
 }
 
 func newMockFile() *bytes.Buffer {
 	return &bytes.Buffer{}
-	// return mockFile, writer
-
-	// file, err := os.OpenFile("test.log", os.O_RDWR|os.O_CREATE, 0666)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// mockFile = make([]byte, 0, 1000)
-	// return mockFile, file
 }
